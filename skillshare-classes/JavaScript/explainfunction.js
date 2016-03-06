70ad236578e8ac7c043ab663bbf2c760c07ec559
Rebecca Snyder <snyderrc@gmail.com>
2016-03-06 16:54:04 -0500

    added normalize

---
+++ skillshare-classes/JavaScript/explainfunction.js
@@ -0,0 +1,27 @@
+// Explanation of Functions
+
+var fullname = function (firstname, secondname) {
+	return firstname + ' ' +secondname; 
+}
+
+var area = function (width, height) {
+	return width * height;
+}
+
+var whatIsMyFavColor = function () {
+
+	return 'blue';
+
+	return true;
+
+}
+
+// it will return the first thing listed and nothing more
+
+var doSomething = function () {
+	console.log("do Something!!!");
+}
+
+// a function can be stored in a variable, or a variable can be stored in a function 
+// a question that you want to return something
+// the brackets make it a function, the method (or what it does) is inside the brackets
